51bf184c308f6f075e296d3a1b38798f241df07a
Thinh Le <mr.thinhbuzz@gmail.com>
2020-04-27 01:13:34 +0700

    - move `hcaptcha_display_captcha` and `hcaptcha_verify_captcha` functions into single file for prevent duplicated code

---
+++ common/hcaptcha.php
@@ -0,0 +1,34 @@
+<?php
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+function hcaptcha_display_captcha()
+{
+    $hcaptcha_api_key = get_option( 'hcaptcha_api_key' );
+    $hcaptcha_theme = get_option( 'hcaptcha_theme' );
+    $hcaptcha_size = get_option( 'hcaptcha_size' );
+    $output = '<div class="h-captcha" data-sitekey="' . $hcaptcha_api_key . '" data-theme="' . $hcaptcha_theme . '" data-size="' . $hcaptcha_size . '"></div>';
+    $output .= wp_nonce_field( 'hcaptcha_lost_password', 'hcaptcha_lost_password_nonce', true, false );
+
+    echo $output;
+}
+
+function hcaptcha_verify_captcha($error)
+{
+    $errorMsg = '<strong>ERROR</strong>: Invalid Captcha';
+    if ( ! isset( $_POST['hcaptcha_lost_password_nonce'] )
+        || ! wp_verify_nonce( $_POST['hcaptcha_lost_password_nonce'], 'hcaptcha_lost_password' )
+        || ! isset( $_POST['h-captcha-response'] ) ) {
+        $error->add( 'invalid_captcha', $errorMsg );
+        return;
+    }
+    $get_hcaptcha_response = htmlspecialchars( sanitize_text_field( $_POST['h-captcha-response'] ) );
+
+    $hcaptcha_secret_key = get_option( 'hcaptcha_secret_key' );
+    $response = wp_remote_get( 'https://hcaptcha.com/siteverify?secret=' . $hcaptcha_secret_key . '&response=' . $get_hcaptcha_response );
+    $response = json_decode( $response['body'], true );
+    if ( false == $response['success'] ) {
+        $error->add( 'invalid_captcha', $errorMsg );
+    }
+}
