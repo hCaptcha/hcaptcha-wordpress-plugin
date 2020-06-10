f2c37cc8ae8ca6d9478d5c47a758482af8d1b69d
kagg-design <info@kagg.eu>
2020-06-10 16:47:11 +0300

    Remove duplicated code in Subscriber. Implement WPCS.

--- subscriber/subscriber.php
+++ subscriber/subscriber.php
@@ -1,30 +1,51 @@
 <?php
+/**
+ * Subscriber file.
+ *
+ * @package hcaptcha-wp
+ */
 
 // If this file is called directly, abort.
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Subscriber form.
+ *
+ * @return string
+ */
+function hcap_subscriber_form() {
+	$output = hcap_form();
+
+	$output .= wp_nonce_field(
+		'hcaptcha_subscriber_form',
+		'hcaptcha_subscriber_form_nonce',
+		true,
+		false
+	);
+
+	return $output;
+}
 
 add_filter( 'sbscrbr_add_field', 'hcap_subscriber_form', 10, 0 );
 
-function hcap_subscriber_form() {
+if ( ! function_exists( 'hcap_subscriber_verify' ) ) {
+	/**
+	 * Verify subscriber captcha.
+	 *
+	 * @param bool $check_result Check result.
+	 *
+	 * @return bool|string
+	 */
+	function hcap_subscriber_verify( $check_result = true ) {
+		$error_message = hcaptcha_get_verify_message( 'hcaptcha_subscriber_form_nonce', 'hcaptcha_subscriber_form' );
+		if ( null === $error_message ) {
+			return $check_result;
+		}
 
-    $hcaptcha_api_key = get_option('hcaptcha_api_key' );
-    $hcaptcha_theme     = get_option("hcaptcha_theme");
-    $hcaptcha_size      = get_option("hcaptcha_size");
-    $output = '<div class="h-captcha" data-sitekey="'.$hcaptcha_api_key.'" data-theme="'.$hcaptcha_theme.'" data-size="'.$hcaptcha_size.'"></div>';
-    $output .= wp_nonce_field( 'hcaptcha_subscriber_form', 'hcaptcha_subscriber_form_nonce', true, false );
-    
-    return $output;
+		return $error_message;
+	}
 }
 
 add_filter( 'sbscrbr_check', 'hcap_subscriber_verify' );
-
-/* check google captcha in subscriber */
-if ( ! function_exists( 'hcap_subscriber_verify' ) ) {
-    function hcap_subscriber_verify( $check_result = true ) {
-	    $errorMessage = hcaptcha_get_verify_message( 'hcaptcha_subscriber_form_nonce', 'hcaptcha_subscriber_form' );
-	    if ( $errorMessage === null ) {
-		    return $check_result;
-	    }
-	    return $errorMessage;
-    }
-}
