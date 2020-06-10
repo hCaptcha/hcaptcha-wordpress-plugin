6ba2422a60a808055d85f3295f505d1e94cca407
kagg-design <info@kagg.eu>
2020-06-10 15:31:19 +0300

    Remove duplicated code in BuddyPress forms. Implement WPCS.

--- bp/bp-create-group.php
+++ bp/bp-create-group.php
@@ -1,33 +1,58 @@
 <?php
+/**
+ * BuddyPress create group form file.
+ *
+ * @package hcaptcha-wp
+ */
 
 // If this file is called directly, abort.
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * BuddyPress group form.
+ */
+function hcap_bp_group_form() {
+	echo '<div class="hcap_buddypress_group_form">';
+
+	hcap_form_display();
+	wp_nonce_field( 'hcaptcha_bp_create_group', 'hcaptcha_bp_create_group_nonce' );
+
+	echo '</div>';
+}
 
 add_action( 'bp_after_group_details_creation_step', 'hcap_bp_group_form' );
-function hcap_bp_group_form() {
-    $hcaptcha_api_key = get_option('hcaptcha_api_key' );
-    $hcaptcha_theme 	= get_option("hcaptcha_theme");
-    $hcaptcha_size 		= get_option("hcaptcha_size");
-    $output = '<div class="h-captcha" data-sitekey="'.$hcaptcha_api_key.'" data-theme="'.$hcaptcha_theme.'" data-size="'.$hcaptcha_size.'"></div>';
-    $output .= wp_nonce_field( 'hcaptcha_bp_create_group', 'hcaptcha_bp_create_group_nonce', true, false );
 
-    //return $content . $output;
-    printf( '<div class="hcap_buddypress_group_form">%s</div>', $output );
+if ( ! function_exists( 'hcap_hcaptcha_bp_group_verify' ) ) {
+
+	/**
+	 * Verify BuddyPress group form captcha.
+	 *
+	 * @param mixed $bp_group BuddyPress group.
+	 *
+	 * @return bool
+	 */
+	function hcap_hcaptcha_bp_group_verify( $bp_group ) {
+		if ( ! bp_is_group_creation_step( 'group-details' ) ) {
+			return false;
+		}
+
+		$error_message = hcaptcha_get_verify_message(
+			'hcaptcha_bp_create_group_nonce',
+			'hcaptcha_bp_create_group'
+		);
+
+		if ( null === $error_message ) {
+			// @todo Looks like a bug, check it.
+			return false;
+		}
+
+		bp_core_add_message( $error_message, 'error' );
+		bp_core_redirect( bp_get_root_domain() . '/' . bp_get_groups_root_slug() . '/create/step/group-details/' );
+
+		return false;
+	}
 }
 
 add_action( 'groups_group_before_save', 'hcap_hcaptcha_bp_group_verify' );
-
-if ( ! function_exists( 'hcap_hcaptcha_bp_group_verify' ) ) {
-	function hcap_hcaptcha_bp_group_verify( $bp_group ) {
-
-        if ( ! bp_is_group_creation_step( 'group-details' ) )
-            return false;
-
-		$errorMessage = hcaptcha_get_verify_message( 'hcaptcha_bp_create_group_nonce', 'hcaptcha_bp_create_group' );
-		if ( $errorMessage === null ) {
-			return false;
-		}
-		bp_core_add_message( $errorMessage, 'error' );
-		bp_core_redirect( bp_get_root_domain() . '/' . bp_get_groups_root_slug() . '/create/step/group-details/' );
-    }
-}
--- bp/bp-register.php
+++ bp/bp-register.php
@@ -1,35 +1,56 @@
 <?php
+/**
+ * BuddyPress register form file.
+ *
+ * @package hcaptcha-wp
+ */
 
 // If this file is called directly, abort.
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-function hcap_display_bp_register(){
-    $hcaptcha_api_key = get_option('hcaptcha_api_key' );
-    $hcaptcha_theme     = get_option("hcaptcha_theme");
-    $hcaptcha_size      = get_option("hcaptcha_size");
+/**
+ * BuddyPress register form.
+ */
+function hcap_display_bp_register() {
+	global $bp;
 
-    $output = '';
-    global $bp;
-    if ( ! empty( $bp->signup->errors['hcaptcha_response_verify'] ) ) {
-        $output .= '<div class="error">';
-        $output .= $bp->signup->errors['hcaptcha_response_verify'];
-        $output .= '</div>';
-    }
-    $output .= '<div class="h-captcha" data-sitekey="'.$hcaptcha_api_key.'" data-theme="'.$hcaptcha_theme.'" data-size="'.$hcaptcha_size.'"></div>';
-    $output .= wp_nonce_field( 'hcaptcha_bp_register', 'hcaptcha_bp_register_nonce', true, false );
+	if ( ! empty( $bp->signup->errors['hcaptcha_response_verify'] ) ) {
+		$output = '<div class="error">';
 
-    echo $output;
+		$output .= $bp->signup->errors['hcaptcha_response_verify'];
+		$output .= '</div>';
+
+		echo wp_kses_post( $output );
+	}
+
+	hcap_form_display();
+	wp_nonce_field( 'hcaptcha_bp_register', 'hcaptcha_bp_register_nonce' );
 }
 
 add_action( 'bp_before_registration_submit_buttons', 'hcap_display_bp_register', 10, 0 );
 
+/**
+ * Verify BuddyPress register captcha.
+ *
+ * @return bool
+ */
 function hcap_verify_bp_register_captcha() {
-    global $bp;
+	global $bp;
 
-	$errorMessage = hcaptcha_get_verify_message( 'hcaptcha_bp_register_nonce', 'hcaptcha_bp_register' );
-	if ( $errorMessage === null ) {
+	$error_message = hcaptcha_get_verify_message(
+		'hcaptcha_bp_register_nonce',
+		'hcaptcha_bp_register'
+	);
+
+	if ( null === $error_message ) {
 		return true;
 	}
-	$bp->signup->errors['hcaptcha_response_verify'] = $errorMessage;
+
+	$bp->signup->errors['hcaptcha_response_verify'] = $error_message;
+
+	return false;
 }
-add_action( 'bp_signup_validate',  'hcap_verify_bp_register_captcha' ); 
+
+add_action( 'bp_signup_validate', 'hcap_verify_bp_register_captcha' );
