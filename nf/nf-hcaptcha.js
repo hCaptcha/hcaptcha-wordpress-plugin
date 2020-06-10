482166f95ee77b03aa9132902126630fe0e05fbd
kagg-design <info@kagg.eu>
2020-06-10 16:41:50 +0300

    Remove duplicated code in Ninja Forms. Implement WPCS.

--- nf/nf-hcaptcha.js
+++ nf/nf-hcaptcha.js
@@ -1,34 +1,34 @@
 var hCaptchaFieldController = Marionette.Object.extend( {
-    initialize: function() {
+	initialize: function() {
 
-        // On the Form Submission's field validaiton...
-        var submitChannel = Backbone.Radio.channel( 'submit' );
-        this.listenTo( submitChannel, 'validate:field', this.updateHcaptcha );
+		// On the Form Submission's field validaiton...
+		var submitChannel = Backbone.Radio.channel( 'submit' );
+		this.listenTo( submitChannel, 'validate:field', this.updateHcaptcha );
 
-        // on the Field's model value change...
-        var fieldsChannel = Backbone.Radio.channel( 'fields' );
-        this.listenTo( fieldsChannel, 'change:modelValue', this.updateHcaptcha );
-    },
+		// on the Field's model value change...
+		var fieldsChannel = Backbone.Radio.channel( 'fields' );
+		this.listenTo( fieldsChannel, 'change:modelValue', this.updateHcaptcha );
+	},
 
-    updateHcaptcha: function( model ) {
+	updateHcaptcha: function( model ) {
 
-        // Only validate a specific fields type.
-        if( 'hcaptcha-for-ninja-forms' != model.get( 'type' ) ) return;
+		// Only validate a specific fields type.
+		if ( 'hcaptcha-for-ninja-forms' !== model.get( 'type' ) ) return;
 
-        // Check if Model has a value
-        if( model.get( 'value' ) ) {
-            // Remove Error from Model
-            Backbone.Radio.channel( 'fields' ).request( 'remove:error', model.get( 'id' ), 'required-error' );
-        } else {
-            var hcap_response = hcaptcha.getResponse();
-            model.set('value', hcap_response);
-        }
-    }
-});
+		// Check if Model has a value
+		if ( model.get( 'value' ) ) {
+			// Remove Error from Model
+			Backbone.Radio.channel( 'fields' ).request( 'remove:error', model.get( 'id' ), 'required-error' );
+		} else {
+			var hcap_response = hcaptcha.getResponse();
+			model.set( 'value', hcap_response );
+		}
+	}
+} );
 
 // On Document Ready...
 jQuery( document ).ready( function( $ ) {
 
-    // Instantiate our custom field's controller, defined above.
-    new hCaptchaFieldController();
-});
+	// Instantiate our custom field's controller, defined above.
+	new hCaptchaFieldController();
+} );
