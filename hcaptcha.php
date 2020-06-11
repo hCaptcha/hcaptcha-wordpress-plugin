92b3bb4088ed9b79da28a155218fdd0928b72772
kagg-design <info@kagg.eu>
2020-06-11 23:00:30 +0300

    Add WP, WC, PHP version numbers. Add kaggdesign as contributor.

--- hcaptcha.php
+++ hcaptcha.php
@@ -7,6 +7,11 @@
  * Author URI: https://hCaptcha.com/
  * Version: 1.5.0
  * Stable tag: 1.5.0
+ * Requires at least: 4.4
+ * Tested up to: 5.4
+ * Requires PHP: 5.6
+ * WC requires at least: 3.0
+ * WC tested up to: 4.2
  *
  * Text Domain: hcaptcha-wp
  * Domain Path: /languages/
