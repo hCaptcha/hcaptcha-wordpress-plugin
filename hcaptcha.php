2f45a8c9f6e2cff9c3e45b4e64699ef8f213c726
e271828- <e271828-@users.noreply.github.com>
2020-07-21 19:17:18 -0700

    bump for 1.5.4 release

--- hcaptcha.php
+++ hcaptcha.php
@@ -5,8 +5,8 @@
  * Description: hCaptcha is a new way to monetize your site traffic while keeping out bots and spam. It is a drop-in replacement for reCAPTCHA.
  * Author: hCaptcha
  * Author URI: https://hCaptcha.com/
- * Version: 1.5.3
- * Stable tag: 1.5.3
+ * Version: 1.5.4
+ * Stable tag: 1.5.4
  * Requires at least: 4.4
  * Tested up to: 5.4
  * Requires PHP: 5.6
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin version.
  */
-define( 'HCAPTCHA_VERSION', '1.5.3' );
+define( 'HCAPTCHA_VERSION', '1.5.4' );
 
 /**
  * Path to the plugin dir.
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: captcha, hcaptcha, make money with captcha, recaptcha, human captcha
 Requires at least: 4.4
 Tested up to: 5.4
 Requires PHP: 5.6  
-Stable tag: 1.5.3
+Stable tag: 1.5.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html  
  
@@ -102,6 +102,10 @@ Please see the hCaptcha privacy policy at:
 
 * WooCommerce Wishlists bug fix
 * text domain updated: better i18n support
+
+= 1.5.4 =
+
+* Added WPForms Lite support
 
 
 
