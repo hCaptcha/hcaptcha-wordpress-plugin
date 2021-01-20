d2def73e4d82887cbdcb97d1210ecccc8c051cd9
Will Presley (sonata) <william.presley.jr+git@gmail.com>
2021-01-20 13:08:44 -0500

    New option for Settings page for 'recaptchacompat'
    Allows adding a URL parameter to stop the replacement of reCaptcha outside of the plugin's included modules.
    Use case: I have sites that include a Constant Contact signup widget, and they include reCaptcha v3 invisible by default with no way to manage the sitekey or secret. Adding this option and enabling it on these sites allows both types of captcha to work properly, even on the same page.

--- hcaptcha.php
+++ hcaptcha.php
@@ -61,11 +61,12 @@ if ( is_admin() ) {
  * Add the hcaptcha script to footer.
  */
 function hcap_captcha_script() {
+    if( get_option( 'hcaptcha_recaptchacompat' ) ) { $compat = '&recaptchacompat=off'; }
 	$dir = plugin_dir_url( __FILE__ );
 	wp_enqueue_style( 'hcaptcha-style', $dir . '/css/style.css', array(), HCAPTCHA_VERSION );
 	wp_enqueue_script(
 		'hcaptcha-script',
-		'//hcaptcha.com/1/api.js?hl=' . get_option( 'hcaptcha_language' ),
+		'//hcaptcha.com/1/api.js?hl=' . get_option( 'hcaptcha_language' ) . $compat,
 		array(),
 		HCAPTCHA_VERSION,
 		true
