383017340feb659071c8d15df89fd5fcefe41683
Will Presley <william.presley.jr+git@gmail.com>
2021-01-20 15:41:36 -0500

    Update common/functions.php as suggested.
    Co-authored-by: e271828- <e271828-@users.noreply.github.com>

--- common/functions.php
+++ common/functions.php
@@ -86,7 +86,7 @@ function hcap_options() {
 			),
 		),
 		'hcaptcha_recaptchacompat'          => array(
-			'label' => __( 'Disable recaptchacompat (stop replacing other instances of reCaptcha with hCaptcha)', 'hcaptcha-for-forms-and-more' ),
+			'label' => __( 'Disable recaptchacompat (stop replacing other instances of reCAPTCHA with hCaptcha)', 'hcaptcha-for-forms-and-more' ),
 			'type'  => 'checkbox',
 		),
 		'hcaptcha_nf_status'                => array(
