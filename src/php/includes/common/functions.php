007312fa2a77769d48da59b60ad31855f3bef459
kagg-design <info@kagg.eu>
2022-01-09 23:37:39 +0200

    Restore support for WP 4.4.

--- src/php/includes/common/functions.php
+++ src/php/includes/common/functions.php
@@ -235,3 +235,25 @@ function hcap_options() {
 		],
 	];
 }
+
+// @codeCoverageIgnoreStart
+if ( ! function_exists( 'wp_doing_ajax' ) ) :
+	/**
+	 * Determines whether the current request is a WordPress Ajax request.
+	 *
+	 * @since 4.7.0
+	 *
+	 * @return bool True if it's a WordPress Ajax request, false otherwise.
+	 */
+	function wp_doing_ajax() {
+		/**
+		 * Filters whether the current request is a WordPress Ajax request.
+		 *
+		 * @since 4.7.0
+		 *
+		 * @param bool $wp_doing_ajax Whether the current request is a WordPress Ajax request.
+		 */
+		return apply_filters( 'wp_doing_ajax', defined( 'DOING_AJAX' ) && DOING_AJAX );
+	}
+endif;
+// @codeCoverageIgnoreEnd
