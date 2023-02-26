53155f8af848efe7e44fdf4b9ec234f6a11f753f
kagg-design <info@kagg.eu>
2023-02-26 12:44:07 +0300

    Add IntegrationsTest.php.

---
+++ .tests/php/unit/Settings/IntegrationsTest.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * IntegrationsTest class file.
+ *
+ * @package HCaptcha\Tests
+ */
+
+// phpcs:disable Generic.Commenting.DocComment.MissingShort
+/** @noinspection PhpUndefinedMethodInspection */
+/** @noinspection PhpArrayShapeAttributeCanBeAddedInspection */
+// phpcs:enable Generic.Commenting.DocComment.MissingShort
+
+namespace HCaptcha\Tests\Unit\Settings;
+
+use HCaptcha\Settings\Abstracts\SettingsBase;
+use HCaptcha\Settings\Integrations;
+use HCaptcha\Tests\Unit\HCaptchaTestCase;
+use Mockery;
+use ReflectionException;
+use tad\FunctionMocker\FunctionMocker;
+use WP_Mock;
+
+/**
+ * Class IntegrationsTest
+ *
+ * @group settings
+ * @group settings-integrations
+ */
+class IntegrationsTest extends HCaptchaTestCase {
+
+	/**
+	 * Test screen_id().
+	 */
+	public function test_screen_id() {
+		$subject = Mockery::mock( Integrations::class )->makePartial()->shouldAllowMockingProtectedMethods();
+
+		self::assertSame( 'settings_page_hcaptcha', $subject->screen_id() );
+	}
+
+	/**
+	 * Test option_group().
+	 */
+	public function test_option_group() {
+		$subject = Mockery::mock( Integrations::class )->makePartial()->shouldAllowMockingProtectedMethods();
+
+		$method = 'option_group';
+		self::assertSame( 'hcaptcha_group', $subject->$method() );
+	}
+
+	/**
+	 * Test option_page().
+	 */
+	public function test_option_page() {
+		$subject = Mockery::mock( Integrations::class )->makePartial()->shouldAllowMockingProtectedMethods();
+
+		$method = 'option_page';
+		self::assertSame( 'hcaptcha', $subject->$method() );
+	}
+
+	/**
+	 * Test option_name().
+	 */
+	public function test_option_name() {
+		$subject = Mockery::mock( Integrations::class )->makePartial()->shouldAllowMockingProtectedMethods();
+
+		$method = 'option_name';
+		self::assertSame( 'hcaptcha_settings', $subject->$method() );
+	}
+
+	/**
+	 * Test page_title().
+	 */
+	public function test_page_title() {
+		$subject = Mockery::mock( Integrations::class )->makePartial()->shouldAllowMockingProtectedMethods();
+
+		$method = 'page_title';
+		self::assertSame( 'Integrations', $subject->$method() );
+	}
+
+	/**
+	 * Test menu_title().
+	 */
+	public function test_menu_title() {
+		$subject = Mockery::mock( Integrations::class )->makePartial()->shouldAllowMockingProtectedMethods();
+
+		$method = 'menu_title';
+		self::assertSame( 'hCaptcha', $subject->$method() );
+	}
+
+	/**
+	 * Test section_title().
+	 */
+	public function test_section_title() {
+		$subject = Mockery::mock( Integrations::class )->makePartial()->shouldAllowMockingProtectedMethods();
+
+		$method = 'section_title';
+		self::assertSame( 'integrations', $subject->$method() );
+	}
+
+	/**
+	 * Test init_form_fields().
+	 *
+	 * @throws ReflectionException ReflectionException.
+	 */
+	public function test_init_form_fields() {
+		$expected = $this->get_test_integrations_form_fields();
+
+		$mock = Mockery::mock( Integrations::class )->makePartial()->shouldAllowMockingProtectedMethods();
+
+		$mock->init_form_fields();
+
+		self::assertSame( $expected, $this->get_protected_property( $mock, 'form_fields' ) );
+	}
+
+	/**
+	 * Test section_callback()
+	 *
+	 * @param string $id       Section id.
+	 * @param string $expected Expected value.
+	 *
+	 * @dataProvider dp_test_section_callback
+	 */
+	public function test_section_callback( $id, $expected ) {
+		WP_Mock::passthruFunction( 'wp_kses_post' );
+
+		$subject = Mockery::mock( Integrations::class )->makePartial()->shouldAllowMockingProtectedMethods();
+
+		ob_start();
+		$subject->section_callback( [ 'id' => $id ] );
+		self::assertSame( $expected, ob_get_clean() );
+	}
+
+	/**
+	 * Data provider for test_section_callback().
+	 *
+	 * @return array
+	 */
+	public function dp_test_section_callback() {
+		return [
+			'disabled' => [
+				Integrations::SECTION_DISABLED,
+				'			<hr class="hcaptcha-disabled-section">
+			<h3>Inactive plugins and themes</h3>
+			',
+			],
+			'default'  => [
+				'',
+				'		<h2>
+			Integrations		</h2>
+		<p>
+			Manage integrations with popular plugins such as Contact Form 7, WPForms, Gravity Forms, and more.		</p>
+		<p>
+			Don\'t see your plugin here? Use the `[hcaptcha]` <a href="https://wordpress.org/plugins/hcaptcha-for-forms-and-more/#does%20the%20%5Bhcaptcha%5D%20shortcode%20have%20arguments%3F" target="_blank">shortcode</a> or <a href="https://github.com/hCaptcha/hcaptcha-wordpress-plugin/issues" target="_blank">request an integration</a>.		</p>
+		<h3>Active plugins and themes</h3>
+		',
+			],
+		];
+	}
+
+	/**
+	 * Test admin_enqueue_scripts().
+	 *
+	 * @throws ReflectionException
+	 */
+	public function test_admin_enqueue_scripts() {
+		$plugin_url     = 'http://test.test/wp-content/plugins/hcaptcha-wordpress-plugin';
+		$plugin_version = '1.0.0';
+		$min_prefix     = '.min';
+
+		$subject = Mockery::mock( Integrations::class )->makePartial();
+		$subject->shouldAllowMockingProtectedMethods();
+		$subject->shouldReceive( 'is_options_screen' )->with()->andReturn( true );
+		$this->set_protected_property( $subject, 'min_prefix', $min_prefix );
+
+		FunctionMocker::replace(
+			'constant',
+			static function ( $name ) use ( $plugin_url, $plugin_version ) {
+				if ( 'HCAPTCHA_URL' === $name ) {
+					return $plugin_url;
+				}
+
+				if ( 'HCAPTCHA_VERSION' === $name ) {
+					return $plugin_version;
+				}
+
+				return '';
+			}
+		);
+
+		WP_Mock::userFunction( 'wp_enqueue_style' )
+			->with(
+				Integrations::HANDLE,
+				$plugin_url . "/assets/css/integrations$min_prefix.css",
+				[ SettingsBase::HANDLE ],
+				$plugin_version
+			)
+			->once();
+
+		$subject->admin_enqueue_scripts();
+	}
+
+	/**
+	 * Test admin_enqueue_scripts() not on own screen.
+	 */
+	public function test_admin_enqueue_scripts_not_on_own_screen() {
+		$subject = Mockery::mock( Integrations::class )->makePartial();
+		$subject->shouldAllowMockingProtectedMethods();
+		$subject->shouldReceive( 'is_options_screen' )->with()->andReturn( false );
+
+		WP_Mock::userFunction( 'wp_enqueue_style' )->never();
+
+		$subject->admin_enqueue_scripts();
+	}
+}
