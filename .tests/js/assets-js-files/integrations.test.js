7fd11c94620f5e830c62f7b9381c4a30a7692c8c
kagg-design <info@kagg.eu>
2023-04-02 11:55:20 +0300

    Jest frame for jQuery files.

---
+++ .tests/js/assets-js-files/integrations.test.js
@@ -0,0 +1,146 @@
+// noinspection JSUnresolvedFunction,JSUnresolvedVariable
+
+import jquery from 'jquery';
+
+global.jQuery = jquery;
+global.$ = jquery;
+
+require('../../../assets/js/integrations.js');
+
+// Mock HCaptchaIntegrationsObject
+global.HCaptchaIntegrationsObject = {
+	ajaxUrl: 'https://test.test/wp-admin/admin-ajax.php',
+	action: 'test_action',
+	nonce: 'test_nonce',
+	activateMsg: 'Activate %s plugin?',
+	deactivateMsg: 'Deactivate %s plugin?',
+};
+
+function getDom() {
+	return `
+      <html lang="en">
+        <body>
+          <div id="hcaptcha-integrations-message"></div>
+          <table class="form-table">
+            <tbody>
+            	<tr class="hcaptcha-integrations-wp-status">
+            		<th scope="row">
+                        <img src="https://test.test/wp-content/plugins/hcaptcha-wordpress-plugin/assets/images/wp-core-logo.png" alt="WP Core Logo">
+                    </th>
+                    <td>
+                    	<fieldset>
+                    		<label for="wp_status_1">
+                    			<input id="wp_status_1" name="hcaptcha_settings[wp_status][]" type="checkbox" value="comment" checked="checked">
+                    			Comment Form
+                            </label>
+                            <br>
+                            <label for="wp_status_2">
+                            	<input id="wp_status_2" name="hcaptcha_settings[wp_status][]" type="checkbox" value="login" checked="checked">
+                            	Login Form
+                            </label>
+                            <br>
+                            <label for="wp_status_3">
+                            	<input id="wp_status_3" name="hcaptcha_settings[wp_status][]" type="checkbox" value="lost_pass" checked="checked">
+                            	Lost Password Form
+                            </label>
+                            <br>
+                            <label for="wp_status_4">
+                            	<input id="wp_status_4" name="hcaptcha_settings[wp_status][]" type="checkbox" value="password_protected" checked="checked">
+                                Post/Page Password Form
+                            </label>
+                            <br>
+                            <label for="wp_status_5">
+                            	<input id="wp_status_5" name="hcaptcha_settings[wp_status][]" type="checkbox" value="register" checked="checked">
+                            	Register Form
+                            </label>
+                            <br>
+                        </fieldset>
+                    </td>
+                </tr>
+			</tbody>
+          </table>
+          <table class="form-table">
+            <tbody>
+            	<tr class="hcaptcha-integrations-acfe-status">
+            		<th scope="row">
+            			<img src="https://test.test/wp-content/plugins/hcaptcha-wordpress-plugin/assets/images/acf-extended-logo.png" alt="ACF Extended Logo">
+                    </th>
+                    <td>
+                    	<fieldset disabled="disabled">
+                    		<label for="acfe_status_1">
+                    			<input id="acfe_status_1" name="hcaptcha_settings[acfe_status][]" type="checkbox" value="form" checked="checked">
+                    			ACF Extended Form
+                            </label>
+                            <br>
+                        </fieldset>
+                    </td>
+                </tr>
+			</tbody>
+          </table>
+        </body>
+      </html>
+    `;
+}
+
+describe('integrations', () => {
+	const successMessage = 'Success.';
+	const errorMessage = 'Error.';
+	let postSpy;
+
+	beforeEach(() => {
+		document.body.innerHTML = getDom();
+
+		// Simulate jQuery.ready event
+		window.hCaptcha.integrations(jQuery);
+
+		const mockSuccessResponse = {
+			data: successMessage,
+			success: true,
+		};
+		const mockErrorResponse = {
+			data: errorMessage,
+			success: false,
+		};
+		const mockPostPromise = {
+			done: jest.fn().mockImplementation((callback) => {
+				callback(mockSuccessResponse);
+				return mockPostPromise;
+			}),
+			fail: jest.fn().mockImplementation((callback) => {
+				callback(mockErrorResponse);
+				return mockPostPromise;
+			}),
+		};
+
+		postSpy = jest.spyOn($, 'post').mockImplementation(() => {
+			const deferred = $.Deferred();
+
+			// To fire the done callback, resolve the deferred object with the mockSuccessResponse
+			deferred.resolve(mockSuccessResponse);
+
+			// Comment the following line if you don't want the fail callback to be fired
+			// deferred.reject(mockErrorResponse);
+
+			return deferred;
+		});
+	});
+
+	afterEach(() => {
+		postSpy.mockRestore();
+	});
+
+	test('clicking on an image sends an AJAX request', () => {
+		const $img = $('.form-table img');
+
+		// No ajax call on click at WP Core.
+		$($img.get(0)).trigger('click');
+		expect(postSpy).not.toHaveBeenCalled();
+
+		jest.spyOn(global, 'confirm').mockReturnValueOnce(true);
+
+		$($img.get(1)).trigger('click');
+		expect(postSpy).toHaveBeenCalled();
+	});
+
+	// Add more tests as needed
+});
