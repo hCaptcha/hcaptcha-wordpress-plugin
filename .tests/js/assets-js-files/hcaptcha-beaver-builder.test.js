3147fd6b00ec9fcc91b0a987bd944b963c5d6649
kagg-design <info@kagg.eu>
2023-04-02 18:59:11 +0300

    hcaptcha-beaver-builder.js test.

---
+++ .tests/js/assets-js-files/hcaptcha-beaver-builder.test.js
@@ -0,0 +1,52 @@
+// noinspection JSUnresolvedFunction,JSUnresolvedVariable
+
+import jquery from 'jquery';
+
+global.jQuery = jquery;
+global.$ = jquery;
+
+describe( 'hCaptcha Beaver Builder', () => {
+	let ajaxPrefilterCallback;
+	let options = {
+		data: '',
+	};
+
+	beforeEach( () => {
+		// Mock jQuery.ajaxPrefilter
+		$.ajaxPrefilter = jest.fn( ( callback ) => {
+			ajaxPrefilterCallback = callback;
+		} );
+
+		document.body.innerHTML = `
+	      <div data-node="123">
+	        <input type="hidden" name="h-captcha-response" value="responseValue">
+	        <input type="hidden" name="hcaptcha_beaver_builder_nonce" value="nonceValue">
+	        <input type="hidden" name="hcaptcha_login_nonce" value="loginNonceValue">
+	      </div>
+	    `;
+
+		require( '../../../assets/js/hcaptcha-beaver-builder.js' );
+	} );
+
+	test( 'appends h-captcha-response and hcaptcha_beaver_builder_nonce when data starts with action=fl_builder_email', () => {
+		options.data = 'action=fl_builder_email&node_id=123';
+		ajaxPrefilterCallback( options );
+		expect( options.data ).toContain( 'h-captcha-response=responseValue' );
+		expect( options.data ).toContain( 'hcaptcha_beaver_builder_nonce=nonceValue' );
+	} );
+
+	test( 'appends h-captcha-response and hcaptcha_login_nonce when data starts with action=fl_builder_login_form_submit', () => {
+		options.data = 'action=fl_builder_login_form_submit&node_id=123';
+		ajaxPrefilterCallback( options );
+		expect( options.data ).toContain( 'h-captcha-response=responseValue' );
+		expect( options.data ).toContain( 'hcaptcha_login_nonce=loginNonceValue' );
+	} );
+
+	test( 'does not append anything when data does not start with any expected action', () => {
+		options.data = 'action=other_action&node_id=123';
+		ajaxPrefilterCallback( options );
+		expect( options.data ).not.toContain( 'h-captcha-response' );
+		expect( options.data ).not.toContain( 'hcaptcha_beaver_builder_nonce' );
+		expect( options.data ).not.toContain( 'hcaptcha_login_nonce' );
+	} );
+} );
