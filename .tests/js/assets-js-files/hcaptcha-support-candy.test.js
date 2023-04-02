50cc1324e7b9897b8639dbe247a80f2258215db0
kagg-design <info@kagg.eu>
2023-04-02 22:43:34 +0300

    hcaptcha-support-candy.js test.

---
+++ .tests/js/assets-js-files/hcaptcha-support-candy.test.js
@@ -0,0 +1,26 @@
+// noinspection JSUnresolvedFunction,JSUnresolvedVariable
+
+import jquery from 'jquery';
+
+global.jQuery = jquery;
+global.$ = jquery;
+
+describe('hCaptcha ajaxStop binding', () => {
+	let hCaptchaBindEvents;
+
+	beforeEach(() => {
+		hCaptchaBindEvents = jest.fn();
+		global.hCaptchaBindEvents = hCaptchaBindEvents;
+
+		require('../../../assets/js/hcaptcha-support-candy.js');
+	});
+
+	afterEach(() => {
+		global.hCaptchaBindEvents.mockRestore();
+	});
+
+	test('hCaptchaBindEvents is called when ajaxStop event is triggered', () => {
+		$(document).trigger('ajaxStop');
+		expect(hCaptchaBindEvents).toHaveBeenCalledTimes(1);
+	});
+});
