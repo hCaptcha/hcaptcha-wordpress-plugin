4ed73fd65051e1aa74e5c0f8e490392b64ceaca7
kagg-design <info@kagg.eu>
2023-05-25 23:31:49 +0300

    Fix WordPress and WooCommerce settings "interfere" #245.

--- src/php/WP/LostPassword.php
+++ src/php/WP/LostPassword.php
@@ -8,6 +8,7 @@
 namespace HCaptcha\WP;
 
 use HCaptcha\Helpers\HCaptcha;
+use HCaptcha\WC\LostPassword as WCLostPassword;
 use WP_Error;
 
 /**
@@ -63,6 +64,15 @@ class LostPassword {
 	 * @return WP_Error
 	 */
 	public function verify( $error ) {
+		if (
+			// Nonce is checked by WC.
+			// phpcs:ignore WordPress.Security.NonceVerification.Missing
+			isset( $_POST['wc_reset_password'] ) &&
+			! hcaptcha()->settings()->is( 'woocommerce_status', 'lost_pass' )
+		) {
+			return $error;
+		}
+
 		$error_message = hcaptcha_get_verify_message_html(
 			self::NONCE,
 			self::ACTION
