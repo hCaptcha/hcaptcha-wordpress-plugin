1f665b103cef465543859e399fbe6111781f1027
kagg-design <info@kagg.eu>
2023-08-15 21:51:40 +0300

    Use ajaxSuccess event to determine which ajax actions is done.

--- assets/js/forminator.js
+++ assets/js/forminator.js
@@ -1,5 +1,11 @@
 /* global jQuery */
 
-jQuery( document ).ajaxStop( function( e ) {
+jQuery( document ).on( 'ajaxSuccess', function( event, xhr, settings ) {
+	const params = new URLSearchParams( settings.data );
+
+	if ( params.get( 'action' ) !== 'forminator_load_form' ) {
+		return;
+	}
+
 	window.hCaptchaBindEvents();
 } );
