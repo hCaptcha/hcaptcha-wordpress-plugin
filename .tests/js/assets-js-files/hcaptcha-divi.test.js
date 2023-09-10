afa194f75557085406bbc6bdd5a580c88a4a9caf
kagg-design <info@kagg.eu>
2023-09-10 20:02:56 +0300

    Replace deprecated ajaxStop in Divi.

--- .tests/js/assets-js-files/hcaptcha-divi.test.js
+++ .tests/js/assets-js-files/hcaptcha-divi.test.js
@@ -20,7 +20,11 @@ describe( 'hCaptcha ajaxStop binding', () => {
 	} );
 
 	test( 'hCaptchaBindEvents is called when ajaxStop event is triggered', () => {
-		$( document ).trigger( 'ajaxStop' );
+		const xhr = {};
+		const settings = {};
+
+		settings.data = '?some_data&et_pb_contactform_submit_0=some_value';
+		$( document ).trigger( 'ajaxSuccess', [ xhr, settings ] );
 		expect( hCaptchaBindEvents ).toHaveBeenCalledTimes( 1 );
 	} );
 } );
--- assets/js/hcaptcha-divi.js
+++ assets/js/hcaptcha-divi.js
@@ -1,5 +1,9 @@
 /* global jQuery */
 
-jQuery( document ).ajaxStop( function() {
+jQuery( document ).on( 'ajaxSuccess', function( event, xhr, settings ) {
+	if ( ! settings.data.includes( 'et_pb_contactform_submit_' ) ) {
+		return;
+	}
+
 	window.hCaptchaBindEvents();
 } );
