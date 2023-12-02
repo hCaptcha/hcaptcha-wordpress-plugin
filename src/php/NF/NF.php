338a68628fc899d88814999f28f0d05715856e18
kagg-design <info@kagg.eu>
2023-12-03 01:35:05 +0300

    Reposition hCaptcha field in NF admin.

--- src/php/NF/NF.php
+++ src/php/NF/NF.php
@@ -51,6 +51,7 @@ class NF {
 		add_action( 'toplevel_page_ninja-forms', [ $this, 'admin_template' ], 11 );
 		add_action( 'nf_admin_enqueue_scripts', [ $this, 'nf_admin_enqueue_scripts' ] );
 		add_filter( 'ninja_forms_register_fields', [ $this, 'register_fields' ] );
+		add_action( 'ninja_forms_loaded', [ $this, 'place_hcaptcha_after_spam_field' ] );
 		add_filter( 'ninja_forms_field_template_file_paths', [ $this, 'template_file_paths' ] );
 		add_action( 'nf_get_form_id', [ $this, 'set_form_id' ] );
 		add_filter( 'ninja_forms_localize_field_hcaptcha-for-ninja-forms', [ $this, 'localize_field' ] );
@@ -149,6 +150,31 @@ class NF {
 		$fields['hcaptcha-for-ninja-forms'] = new Field();
 
 		return $fields;
+	}
+
+	/**
+	 * Place hCaptcha field after spam field.
+	 *
+	 * @return void
+	 */
+	public function place_hcaptcha_after_spam_field() {
+		$hcaptcha_key   = 'hcaptcha-for-ninja-forms';
+		$fields         = Ninja_Forms()->fields;
+		$hcaptcha_value = $fields[ $hcaptcha_key ];
+
+		unset( $fields[ $hcaptcha_key ] );
+
+		$index = array_search( 'spam', array_keys( $fields ), true );
+
+		if ( false === $index ) {
+			return;
+		}
+
+		Ninja_Forms()->fields = array_merge(
+			array_slice( $fields, 0, $index + 1 ),
+			[ $hcaptcha_key => $hcaptcha_value ],
+			array_slice( $fields, $index + 1 )
+		);
 	}
 
 	/**
