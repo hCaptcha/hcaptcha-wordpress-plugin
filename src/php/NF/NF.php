bc40467af7bdfe8a9795855923d6f007b6764220
kagg-design <info@kagg.eu>
2024-03-24 23:18:44 +0200

    Fix Force with NF.

--- src/php/NF/NF.php
+++ src/php/NF/NF.php
@@ -268,7 +268,7 @@ class NF {
 		wp_enqueue_script(
 			self::HANDLE,
 			HCAPTCHA_URL . "/assets/js/hcaptcha-nf$min.js",
-			[ 'jquery', Main::HANDLE, 'nf-front-end', 'nf-front-end-deps' ],
+			[ 'jquery', Main::HANDLE, 'nf-front-end', 'nf-front-end-deps', 'wp-hooks' ],
 			HCAPTCHA_VERSION,
 			true
 		);
