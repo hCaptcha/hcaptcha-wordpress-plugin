ba3304da3aa80cf66f4b67ab671e03094e63bff6
kagg-design <info@kagg.eu>
2024-05-30 22:17:30 +0300

    Add tests for Events.

--- .codeception/_data/dumps/db.sql
+++ .codeception/_data/dumps/db.sql
@@ -15,6 +15,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
 --
 -- Table structure for table `wp_commentmeta`
 --
