09061d6e248382915bf8cc4e5f9e2ce5d147009a
kagg-design <info@kagg.eu>
2024-06-16 10:35:44 +0300

    Update composer packages.

---
+++ lib/matthiasmullie/minify/src/Exception.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Base Exception.
+ *
+ * @deprecated Use Exceptions\BasicException instead
+ *
+ * @author Matthias Mullie <minify@mullie.eu>
+ */
+namespace HCaptcha\Vendor\MatthiasMullie\Minify;
+
+/**
+ * Base Exception Class.
+ *
+ * @deprecated Use Exceptions\BasicException instead
+ *
+ * @author Matthias Mullie <minify@mullie.eu>
+ */
+abstract class Exception extends \Exception
+{
+}
