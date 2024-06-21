ea1b795065c9080f1c0c400707aa9bd3480ed96f
kagg-design <info@kagg.eu>
2024-06-21 15:43:29 +0300

    WC compatibility 9.0.

--- hcaptcha.php
+++ hcaptcha.php
@@ -21,7 +21,7 @@
  * Domain Path:          /languages/
  *
  * WC requires at least: 3.0
- * WC tested up to:      8.9
+ * WC tested up to:      9.0
  */
 
 // phpcs:ignore Generic.Commenting.DocComment.MissingShort
