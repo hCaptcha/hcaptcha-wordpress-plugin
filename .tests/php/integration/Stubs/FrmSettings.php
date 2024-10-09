e32f41a44217f76b6b6b6ec28919bf7b6eb18419
kagg-design <info@kagg.eu>
2024-10-09 21:27:57 +0300

    Cover Formidable Forms by tests 100%.

---
+++ .tests/php/integration/Stubs/FrmSettings.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * FrmSettings stub file.
+ *
+ * @package HCaptcha\Tests
+ */
+
+// phpcs:ignore Generic.Commenting.DocComment.MissingShort
+/** @noinspection PhpIllegalPsrClassPathInspection */
+
+/**
+ * Class FrmSettings.
+ */
+class FrmSettings {
+
+	/**
+	 * Active captcha.
+	 *
+	 * @var string
+	 */
+	public $active_captcha;
+
+	/**
+	 * The hCaptcha public key.
+	 *
+	 * @var string
+	 */
+	public $hcaptcha_pubkey;
+
+	/**
+	 * The hCaptcha private key.
+	 *
+	 * @var string
+	 */
+	public $hcaptcha_privkey;
+}
