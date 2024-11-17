bd0c78aa842bd29456e4180a9d5a96a622769329
kagg-design <info@kagg.eu>
2024-11-17 15:47:56 +0200

    Fix race condition in Mailchimp admin.

--- assets/js/admin-mailchimp.js
+++ assets/js/admin-mailchimp.js
@@ -8,97 +8,90 @@
  */
 
 document.addEventListener( 'DOMContentLoaded', function() {
-	const fields = document.querySelector( 'div.mc4wp-form-fields' );
+	/**
+	 * Parse shortcode in WordPress style.
+	 *
+	 * @param {string} content The content to parse.
+	 *
+	 * @return {Object|null} The parsed attributes or null if the content does not contain a shortcode.
+	 */
+	function parseShortcode( content ) {
+		const scRegex = /\[hcaptcha\s+([^\]]+)]/;
+		const match = content.match( scRegex );
 
-	if ( ! fields ) {
-		return;
-	}
-
-	fields.addEventListener( 'mc4wp-refresh', function() {
-		/**
-		 * Parse shortcode in WordPress style.
-		 *
-		 * @param {string} content The content to parse.
-		 *
-		 * @return {Object|null} The parsed attributes or null if the content does not contain a shortcode.
-		 */
-		function parseShortcode( content ) {
-			const scRegex = /\[hcaptcha\s+([^\]]+)]/;
-			const match = content.match( scRegex );
-
-			if ( ! match ) {
-				return null;
-			}
-
-			const attsString = match[ 1 ];
-			let atts = {};
-			const attsRegex = /(\w+)="([^"]*)"/g;
-			let attsMatch;
-
-			while ( ( attsMatch = attsRegex.exec( attsString ) ) !== null ) {
-				atts[ attsMatch[ 1 ] ] = attsMatch[ 2 ];
-			}
-
-			const params = hCaptcha.getParams();
-			const pairs = {
-				action: 'hcaptcha_action',
-				name: 'hcaptcha_nonce',
-				auto: false,
-				force: params?.force || false,
-				theme: params?.theme || 'light',
-				size: params?.size || 'normal',
-				id: [],
-				protect: true,
-			};
-
-			// Merge the default attributes with the parsed attributes.
-			atts = Object.assign( pairs, atts );
-
-			// Add the full shortcode string to the attributes.
-			atts[ 0 ] = match[ 0 ];
-
-			return atts;
+		if ( ! match ) {
+			return null;
 		}
 
-		/**
-		 * Get hCaptcha form.
-		 *
-		 * @param {Object} args The attributes.
-		 */
-		function form( args ) {
-			const params = hCaptcha.getParams();
-			const defaults = {
-				action: '',
-				name: '',
-				auto: false,
-				force: params?.force || false,
-				theme: params?.theme || 'light',
-				size: params?.size || 'normal',
-				id: [],
-				protect: true,
-			};
-			const allowedThemes = [ 'light', 'dark', 'auto' ];
-			const allowedSizes = [ 'normal', 'compact', 'invisible' ];
+		const attsString = match[ 1 ];
+		let atts = {};
+		const attsRegex = /(\w+)="([^"]*)"/g;
+		let attsMatch;
 
-			args = Object.assign( defaults, args );
+		while ( ( attsMatch = attsRegex.exec( attsString ) ) !== null ) {
+			atts[ attsMatch[ 1 ] ] = attsMatch[ 2 ];
+		}
 
-			args.action = String( args.action );
-			args.name = String( args.name );
-			args.auto = Boolean( args.auto );
-			args.force = Boolean( args.force );
-			args.theme = allowedThemes.includes( args.theme ) ? String( args.theme ) : 'light';
-			args.size = allowedSizes.includes( args.size ) ? String( args.size ) : 'normal';
-			args.id = Array.isArray( args.id ) ? args.id : [ args.id ];
-			args.protect = Boolean( args.protect );
+		const params = hCaptcha.getParams();
+		const pairs = {
+			action: 'hcaptcha_action',
+			name: 'hcaptcha_nonce',
+			auto: false,
+			force: params?.force || false,
+			theme: params?.theme || 'light',
+			size: params?.size || 'normal',
+			id: [],
+			protect: true,
+		};
 
-			// Widget is always the same in the admin.
-			const widget = HCaptchaMailchimpObject.widget;
+		// Merge the default attributes with the parsed attributes.
+		atts = Object.assign( pairs, atts );
 
-			if ( ! args.protect ) {
-				return '';
-			}
+		// Add the full shortcode string to the attributes.
+		atts[ 0 ] = match[ 0 ];
 
-			const hCaptchaTag = `
+		return atts;
+	}
+
+	/**
+	 * Get hCaptcha form.
+	 *
+	 * @param {Object} args The attributes.
+	 */
+	function form( args ) {
+		const params = hCaptcha.getParams();
+		const defaults = {
+			action: '',
+			name: '',
+			auto: false,
+			force: params?.force || false,
+			theme: params?.theme || 'light',
+			size: params?.size || 'normal',
+			id: [],
+			protect: true,
+		};
+		const allowedThemes = [ 'light', 'dark', 'auto' ];
+		const allowedSizes = [ 'normal', 'compact', 'invisible' ];
+
+		args = Object.assign( defaults, args );
+
+		args.action = String( args.action );
+		args.name = String( args.name );
+		args.auto = Boolean( args.auto );
+		args.force = Boolean( args.force );
+		args.theme = allowedThemes.includes( args.theme ) ? String( args.theme ) : 'light';
+		args.size = allowedSizes.includes( args.size ) ? String( args.size ) : 'normal';
+		args.id = Array.isArray( args.id ) ? args.id : [ args.id ];
+		args.protect = Boolean( args.protect );
+
+		// Widget is always the same in the admin.
+		const widget = HCaptchaMailchimpObject.widget;
+
+		if ( ! args.protect ) {
+			return '';
+		}
+
+		const hCaptchaTag = `
 				<h-captcha
 					class="h-captcha"
 					data-sitekey="${ params?.sitekey ?? '' }"
@@ -109,21 +102,41 @@ document.addEventListener( 'DOMContentLoaded', function() {
 				</h-captcha>
 				`;
 
-			const nonceField = HCaptchaMailchimpObject.nonceField;
+		const nonceField = HCaptchaMailchimpObject.nonceField;
 
-			return widget + hCaptchaTag + nonceField;
-		}
+		return widget + hCaptchaTag + nonceField;
+	}
 
-		const parsedAtts = parseShortcode( fields.innerHTML );
-
-		// If the shortcode is not found, return.
-		if ( ! parsedAtts ) {
+	/**
+	 * Add hCaptcha to the form.
+	 */
+	function addHCaptcha() {
+		if ( ! ( mc4wpRefreshFired && hCaptchaLoadedFired ) ) {
 			return;
 		}
 
-		const args = { ...parsedAtts };
+		const parsedAtts = parseShortcode( fields.innerHTML );
+		let args;
+		let search;
 
-		delete args[ 0 ];
+		if ( parsedAtts ) {
+			// Replace the shortcode with the hCaptcha form.
+			args = { ...parsedAtts };
+			search = args[ 0 ];
+
+			delete args[ 0 ];
+		} else {
+			// Add the hCaptcha form before submit button.
+			args = {};
+
+			const match = fields.innerHTML.match( /<p>\s*?<input .*?type="submit".*?>\s*<\/p>/s );
+
+			search = match ? match[ 0 ] : '';
+		}
+
+		if ( ! search ) {
+			return;
+		}
 
 		// We cannot use non-standard nonce without making an ajax call.
 		args.action = HCaptchaMailchimpObject.action;
@@ -131,6 +144,29 @@ document.addEventListener( 'DOMContentLoaded', function() {
 
 		const hcapForm = form( args );
 
-		fields.innerHTML = fields.innerHTML.replace( parsedAtts[ 0 ], hcapForm );
+		if ( parsedAtts ) {
+			fields.innerHTML = fields.innerHTML.replace( search, hcapForm );
+		} else {
+			fields.innerHTML = fields.innerHTML.replace( search, `<p>${ hcapForm }${ search }</p>` );
+		}
+	}
+
+	const fields = document.querySelector( 'div.mc4wp-form-fields' );
+
+	if ( ! fields ) {
+		return;
+	}
+
+	let mc4wpRefreshFired = false;
+	let hCaptchaLoadedFired = false;
+
+	fields.addEventListener( 'mc4wp-refresh', function() {
+		mc4wpRefreshFired = true;
+		addHCaptcha();
+	} );
+
+	document.addEventListener( 'hCaptchaLoaded', function() {
+		hCaptchaLoadedFired = true;
+		addHCaptcha();
 	} );
 } );
