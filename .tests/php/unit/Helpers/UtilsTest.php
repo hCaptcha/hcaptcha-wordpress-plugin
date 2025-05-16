c69cf22f8169256ebb98c4919368f17c0c61c3e1
KAGG Design <info@kagg.eu>
2025-05-16 16:07:35 +0300

    Move remove_action_regex() to a separate class.

---
+++ .tests/php/unit/Helpers/UtilsTest.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * UtilsTest class file.
+ *
+ * @package HCaptcha\Tests
+ */
+
+// phpcs:disable Generic.Commenting.DocComment.MissingShort
+/** @noinspection PhpLanguageLevelInspection */
+/** @noinspection PhpUndefinedClassInspection */
+// phpcs:enable Generic.Commenting.DocComment.MissingShort
+
+namespace HCaptcha\Tests\Unit\Helpers;
+
+use HCaptcha\Helpers\Utils;
+use HCaptcha\Tests\Unit\HCaptchaTestCase;
+use Mockery;
+use WP_Mock;
+
+/**
+ * Test Utils class.
+ *
+ * @group helpers
+ * @group helpers-utils
+ */
+class UtilsTest extends HCaptchaTestCase {
+	/**
+	 * Test remove_action_regex().
+	 *
+	 * @return void
+	 */
+	public function test_remove_action_regex(): void {
+		global $wp_filter;
+
+		for ( $i = 1; $i <= 6; $i++ ) {
+			$action[ $i ] = [
+				'name' . $i => [
+					'function'      => [ 'SomeClass' . $i, 'some_method' . $i ],
+					'accepted_args' => 1,
+				],
+			];
+		}
+
+		$init_callbacks = (object) [
+			'callbacks' => [
+				10 => array_merge( $action[1], $action[2] ),
+				20 => $action[3],
+			],
+		];
+		$ast_callbacks  = (object) [
+			'callbacks' => [
+				0 => $action[4],
+				5 => array_merge( $action[5], $action[6] ),
+			],
+		];
+
+		$callback_pattern = '/^Avada/';
+
+		$subject = Mockery::mock( Utils::class )->makePartial();
+
+		$subject->shouldAllowMockingProtectedMethods();
+
+		foreach ( $init_callbacks->callbacks as $priority => $actions ) {
+			foreach ( $actions as $action ) {
+				$subject->shouldReceive( 'maybe_remove_action_regex' )
+					->with( $callback_pattern, 'init', $action, $priority )->once();
+			}
+		}
+
+		foreach ( $ast_callbacks->callbacks as $priority => $actions ) {
+			foreach ( $actions as $action ) {
+				$subject->shouldReceive( 'maybe_remove_action_regex' )
+					->with( $callback_pattern, 'after_switch_theme', $action, $priority )->once();
+			}
+		}
+
+		WP_Mock::userFunction( 'current_action' )->andReturn( 'init' );
+
+		// No actions.
+		$subject->remove_action_regex( $callback_pattern );
+
+		// No callbacks for 'init'.
+		// phpcs:disable WordPress.WP.GlobalVariablesOverride.Prohibited
+		$wp_filter['init'] = [];
+
+		$subject->remove_action_regex( $callback_pattern );
+
+		// Two callbacks for 'init'.
+		// phpcs:disable WordPress.WP.GlobalVariablesOverride.Prohibited
+		$wp_filter['init'] = $init_callbacks;
+
+		$subject->remove_action_regex( $callback_pattern );
+
+		// Two callbacks for 'after_switch_theme'.
+		// phpcs:disable WordPress.WP.GlobalVariablesOverride.Prohibited
+		$wp_filter['after_switch_theme'] = $ast_callbacks;
+
+		$subject->remove_action_regex( $callback_pattern, 'after_switch_theme' );
+		// phpcs:enable WordPress.WP.GlobalVariablesOverride.Prohibited
+	}
+
+	/**
+	 * Test maybe_remove_action_regex().
+	 *
+	 * @return void
+	 */
+	public function test_maybe_remove_action_regex(): void {
+		$callback_pattern = '/^Avada/';
+		$hook_name        = 'after_switch_theme';
+		$action           = [];
+		$priority         = 10;
+
+		$subject = Mockery::mock( Utils::class )->makePartial();
+
+		$subject->shouldAllowMockingProtectedMethods();
+
+		// Callback is closure.
+		$action['function'] = static function () {
+			return true;
+		};
+
+		$subject->maybe_remove_action_regex( $callback_pattern, $hook_name, $action, $priority );
+
+		// Callback is array. Class is an object.
+		$action['function'] = [ $this, 'some_method' ];
+
+		$subject->maybe_remove_action_regex( $callback_pattern, $hook_name, $action, $priority );
+
+		// Callback is array. Class is a string.
+		$action['function'] = [ 'SomeClass', 'some_method' ];
+
+		$subject->maybe_remove_action_regex( $callback_pattern, $hook_name, $action, $priority );
+
+		// Callback is a string.
+		$action['function'] = 'some_function';
+
+		$subject->maybe_remove_action_regex( $callback_pattern, $hook_name, $action, $priority );
+
+		// Callback is matched class and method.
+		$action['function'] = [ 'AvadaClass', 'some_method' ];
+
+		WP_Mock::userFunction( 'remove_action' )->with( $hook_name, $action['function'], $priority )->once();
+
+		$subject->maybe_remove_action_regex( $callback_pattern, $hook_name, $action, $priority );
+	}
+}
---
+++ src/php/Helpers/Utils.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Utils class file.
+ *
+ * @package hcaptcha-wp
+ */
+
+namespace HCaptcha\Helpers;
+
+use Closure;
+
+/**
+ * Class Utils.
+ */
+class Utils {
+	/**
+	 * Instance.
+	 *
+	 * @var Utils|null
+	 */
+	protected static ?Utils $instance = null;
+
+	/**
+	 * Get Utils Instance.
+	 *
+	 * @return self
+	 */
+	public static function instance(): Utils {
+		if ( self::$instance === null ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Remove action or filter.
+	 *
+	 * @param string $callback_pattern Callback pattern to match. A regex matching to
+	 *                                 SomeNameSpace\SomeClass::some_method.
+	 * @param string $hook_name        Action name.
+	 *
+	 * @return void
+	 */
+	public function remove_action_regex( string $callback_pattern, string $hook_name = '' ): void {
+		global $wp_filter;
+
+		$hook_name = $hook_name ?: current_action();
+		$hooks     = $wp_filter[ $hook_name ] ?? null;
+		$callbacks = $hooks->callbacks ?? [];
+
+		foreach ( $callbacks as $priority => $actions ) {
+			foreach ( $actions as $action ) {
+				$this->maybe_remove_action_regex( $callback_pattern, $hook_name, $action, $priority );
+			}
+		}
+	}
+
+	/**
+	 * Maybe remove action.
+	 *
+	 * @param string $callback_pattern Callback pattern to match. A regex matching to
+	 *                                 SomeNameSpace\SomeClass::some_method.
+	 * @param string $hook_name        Hook name.
+	 * @param array  $action           Action data.
+	 * @param int    $priority         Priority.
+	 *
+	 * @return void
+	 */
+	protected function maybe_remove_action_regex( string $callback_pattern, string $hook_name, array $action, int $priority ): void {
+		$callback = $action['function'] ?? '';
+
+		if ( $callback instanceof Closure ) {
+			return;
+		}
+
+		if ( is_array( $callback ) ) {
+			$callback_class  = is_object( $callback[0] ) ? get_class( $callback[0] ) : (string) $callback[0];
+			$callback_method = (string) $callback[1];
+			$callback_name   = $callback_class . '::' . $callback_method;
+		} else {
+			$callback_name = (string) $callback;
+		}
+
+		if ( ! preg_match( $callback_pattern, $callback_name ) ) {
+			return;
+		}
+
+		remove_action( $hook_name, $callback, $priority );
+	}
+}
