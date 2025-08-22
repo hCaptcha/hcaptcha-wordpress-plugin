e2053b399a608fc45625b058f5a98d345aa9fcc8
KAGG Design <info@kagg.eu>
2025-08-22 21:39:48 +0300

    Update FST token upon sending a form.

--- assets/js/hcaptcha-fst.js
+++ assets/js/hcaptcha-fst.js
@@ -1,4 +1,4 @@
-/* global HCaptchaFSTObject */
+/* global jQuery, HCaptchaFSTObject */
 
 /**
  * @param HCaptchaFSTObject.absPath
@@ -8,32 +8,70 @@
  * @param HCaptchaFSTObject.issueTokenAction
  */
 
-document.addEventListener( 'hCaptchaLoaded', function() {
-	( async function() {
-		const bodyClassName = document.body.className;
-		let postId = bodyClassName.match( /post-id-(\d+)/ )?.[ 1 ] ?? '';
-		postId = bodyClassName.match( /page-id-(\d+)/ )?.[ 1 ] ?? postId;
-		const formBody = new URLSearchParams();
+/**
+ * The 'Form Submit Token' script.
+ *
+ * @param {Document} document The document instance.
+ */
+const fst = window.hCaptchaFST || ( function( document ) {
+	/**
+	 * Public functions and properties.
+	 *
+	 * @type {Object}
+	 */
+	const app = {
+		init() {
+			let hCaptchaLoaded;
 
-		formBody.set( 'action', HCaptchaFSTObject.issueTokenAction );
-		formBody.set( 'absPath', HCaptchaFSTObject.absPath );
-		formBody.set( 'ajaxPath', HCaptchaFSTObject.ajaxPath );
-		formBody.set( 'postId', postId );
+			document.addEventListener( 'hCaptchaAfterBindEvents', function() {
+				if ( ! hCaptchaLoaded ) {
+					return;
+				}
 
-		const res = await fetch( HCaptchaFSTObject.ajaxUrl, {
-			method: 'POST',
-			credentials: 'same-origin',
-			cache: 'no-store',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-			},
-			body: formBody.toString(),
-		} );
-		const body = await res.json();
-		const token = body?.data?.token ?? '';
+				app.getToken();
+			} );
 
-		document.querySelectorAll( '[name="hcap_fst_token"]' ).forEach( ( element ) => {
-			element.value = token;
-		} );
-	}() );
-} );
+			document.addEventListener( 'hCaptchaLoaded', function() {
+				app.getToken();
+
+				hCaptchaLoaded = true;
+			} );
+		},
+
+		getToken() {
+			( async function() {
+				const bodyClassName = document.body.className;
+				let postId = bodyClassName.match( /post-id-(\d+)/ )?.[ 1 ] ?? '';
+				postId = bodyClassName.match( /page-id-(\d+)/ )?.[ 1 ] ?? postId;
+				const formBody = new URLSearchParams();
+
+				formBody.set( 'action', HCaptchaFSTObject.issueTokenAction );
+				formBody.set( 'absPath', HCaptchaFSTObject.absPath );
+				formBody.set( 'ajaxPath', HCaptchaFSTObject.ajaxPath );
+				formBody.set( 'postId', postId );
+
+				const res = await fetch( HCaptchaFSTObject.ajaxUrl, {
+					method: 'POST',
+					credentials: 'same-origin',
+					cache: 'no-store',
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+					},
+					body: formBody.toString(),
+				} );
+				const body = await res.json();
+				const token = body?.data?.token ?? '';
+
+				document.querySelectorAll( '[name="hcap_fst_token"]' ).forEach( ( element ) => {
+					element.value = token;
+				} );
+			}() );
+		},
+	};
+
+	return app;
+}( document, window, jQuery ) );
+
+window.hCaptchaFST = fst;
+
+fst.init();
