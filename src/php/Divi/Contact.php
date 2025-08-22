74f6147de71879a7565b8994987fd0a4211b04d9
KAGG Design <info@kagg.eu>
2025-08-22 10:47:32 +0300

    Remove hCaptcha fields from Divi processing.

--- src/php/Divi/Contact.php
+++ src/php/Divi/Contact.php
@@ -142,7 +142,7 @@ class Contact {
 
 		// Check that the form was submitted and the et_pb_contact_et_number field is empty to protect from spam.
 		if ( $nonce_result && isset( $_POST[ $submit_field ] ) && empty( $_POST[ $number_field ] ) ) {
-			// Remove hcaptcha from current form fields, because Divi compares current and submitted fields.
+			// Remove hCaptcha fields from current form fields, because Divi compares current and submitted fields.
 			$current_form_field  = 'et_pb_contact_email_fields_' . $this->render_count;
 			$current_form_fields = filter_input( INPUT_POST, $current_form_field, FILTER_SANITIZE_FULL_SPECIAL_CHARS );
 			$fields_data_array   = [];
@@ -153,7 +153,7 @@ class Contact {
 				$fields_data_array            = array_filter(
 					$fields_data_array,
 					static function ( $item ) {
-						return false === strpos( $item['field_id'], 'captcha' );
+						return ! preg_match( '/captcha|hcap_hp_|hcap_fst_token/', $item['field_id'] );
 					}
 				);
 				$fields_data_json             = wp_json_encode( $fields_data_array, JSON_UNESCAPED_UNICODE );
