aff5afca5d8576360257d97fce15d3d5de2a093f
KAGG Design <info@kagg.eu>
2025-09-20 14:48:08 +0300

    Fix integration with Paid Membership Pro Login.

--- src/php/PaidMembershipsPro/Login.php
+++ src/php/PaidMembershipsPro/Login.php
@@ -8,6 +8,8 @@
 namespace HCaptcha\PaidMembershipsPro;
 
 use HCaptcha\Abstracts\LoginBase;
+use HCaptcha\Helpers\Request;
+use HCaptcha\Helpers\Utils;
 
 /**
  * Class Login.
@@ -22,7 +24,28 @@ class Login extends LoginBase {
 	protected function init_hooks(): void {
 		parent::init_hooks();
 
+		add_filter( 'pmpro_pages_custom_template_path', [ $this, 'filter_custom_template_path' ], 10, 2 );
 		add_filter( 'pmpro_pages_shortcode_login', [ $this, 'add_pmpro_captcha' ] );
+	}
+
+	/**
+	 * Filter custom template path.
+	 *
+	 * @param array|mixed $default_templates Default templates.
+	 * @param string      $page_name         Page name.
+	 *
+	 * @return mixed
+	 */
+	public function filter_custom_template_path( $default_templates, string $page_name ) {
+		if ( 'login' === $page_name ) {
+			// Remove LoginOut action to avoid hCaptcha duplicating.
+			Utils::instance()->remove_action_regex(
+				'/LoginOut::add_wp_login_out_hcaptcha/',
+				'login_form_middle'
+			);
+		}
+
+		return $default_templates;
 	}
 
 	/**
@@ -30,32 +53,27 @@ class Login extends LoginBase {
 	 *
 	 * @param string|mixed $content Content of the PMPro login page.
 	 *
-	 * @return string|mixed
+	 * @return string
 	 */
-	public function add_pmpro_captcha( $content ) {
+	public function add_pmpro_captcha( $content ): string {
+		$content = (string) $content;
+
 		if ( ! $this->is_login_limit_exceeded() ) {
 			return $content;
 		}
 
-		$content = (string) $content;
-
-		// phpcs:disable WordPress.Security.NonceVerification.Recommended
-		$action = isset( $_GET['action'] ) ?
-			sanitize_text_field( wp_unslash( $_GET['action'] ) ) :
-			'';
-		// phpcs:enable WordPress.Security.NonceVerification.Recommended
-
+		$action         = Request::filter_input( INPUT_GET, 'action' );
 		$error_messages = hcap_get_error_messages();
 
 		if ( array_key_exists( $action, $error_messages ) ) {
-			$search        = '<div class="pmpro_login_wrap">';
+			$search        = '<div class="pmpro_card pmpro_login_wrap">';
 			$error_message = '<div class="pmpro_message pmpro_error">' . $error_messages[ $action ] . '</div>';
 			$content       = str_replace( $search, $error_message . $search, $content );
 		}
 
 		$hcaptcha = '';
 
-		// Check the login status because class is always loading when PMPro is active.
+		// Check the login status because the class is always loading when PMPro is active.
 		if ( hcaptcha()->settings()->is( 'paid_memberships_pro_status', 'login' ) ) {
 			ob_start();
 			$this->add_captcha();
