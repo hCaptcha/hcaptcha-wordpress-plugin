2eda15001f49b57fb7fc62e9fbf07eb35f6d533d
KAGG Design <info@kagg.eu>
2025-12-14 11:38:08 +0200

    Fix double rendering of the hCaptcha widget on the Elementor Form.

--- src/js/hcaptcha/hcaptcha.js
+++ src/js/hcaptcha/hcaptcha.js
@@ -631,8 +631,6 @@ class HCaptcha {
 			this.moveHP( formElement );
 			this.addFSTToken( formElement );
 
-			console.log( 'render hcaptcha' );
-
 			// Render or re-render.
 			hcaptchaElement.innerHTML = '';
 
